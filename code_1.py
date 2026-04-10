f38feb1e5b34ea42bb26f86c75b8a7667311640f
azra <azra@azra-MacBook-Air-2.local>
2026-04-10 22:10:38 +0300

    code file was changed

--- code_1.py
+++ code_1.py
@@ -1,3 +1,15 @@
+print("Hello world!")
+
+# New line is added
+print("Hey! I'm changing the file!")
+
+import time
+
+for i in range(5, -1, -1):
+    print("--->", i)
+    time.sleep(1)
+
+print("Rocket was launched!")
 print('Hello world!')
 # New line is added
 print("Hey! I'm changing the file!")
